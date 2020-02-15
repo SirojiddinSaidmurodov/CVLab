6e762a974ad674e00e01f27c15d6d5b9dcf4f075
Sirojiddin Saidmurodov <48718210+SirojiddinSaidmurodov@users.noreply.github.com>
2020-02-15 10:27:41 +0300

    Update homography.py

--- HumanTracking/homography.py
+++ HumanTracking/homography.py
@@ -3,6 +3,7 @@ import cv2 as cv
 
 
 def calc_homograph():
+    # you should put at least 4 corresponding points
     pts_src = np.array([[306, 365], [933, 386], [830, 570], [335, 560]])
     pts_dst = np.array([[5, 325], [5, 5], [205, 85], [205, 250]])
     homograph, status = cv.findHomography(pts_src, pts_dst)
